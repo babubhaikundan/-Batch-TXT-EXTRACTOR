6bf5eb33bb4398aea0d7f25adcb5d76ea65be55a
𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧 😎 <kundanvirajbhatt@gmail.com>
2025-06-10 14:52:41 +0530

    config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5096393058"))  # Your Telegram user I
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://kundanvirajbhatt:YA0cwX04q2PSMF8h@cluster0.g5md3iu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))  # Telegram channel ID (with -100 prefix)
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002616654030"))  # Telegram channel ID (with -100 prefix)
 
-PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "")  # Optional here you'll get all logs
+PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002616654030")  # Optional here you'll get all logs
